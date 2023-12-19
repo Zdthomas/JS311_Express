825048039155b7862a8670418d445ea5346ac40d
Zdthomas <thomaszachary@live.com>
2023-12-19 00:49:35 -0600

    Hub

--- index.js
+++ index.js
@@ -5,8 +5,95 @@ const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-/* BEGIN - create routes here */
+app.use(express.json());
 
+
+
+/* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+
+  res.json(users)
+
+})
+
+app.get('/users/:id', (req, res) => {
+
+  const user = users.find((user) => user._id == req.params.id)
+
+
+
+  res.json(user);
+
+
+})
+
+// app.push('/users/:id', (req, res) => {
+
+app.delete('/users/:id', (req, res) => {
+
+
+  const user = users.findIndex((user) => user._id == req.params.id)
+
+
+  if (user !== -1) {
+
+    const deleteUser = users.splice(user, 1);
+
+    res.json(deleteUser);
+
+  }
+
+  // new key value = isActive: False
+  
+  // res.send(deleted)
+
+
+})
+
+// })
+
+
+app.post('/users', (req, res) => {
+
+  console.log('req.body', req.body);
+
+  users.push({
+
+    ...req.body,
+
+    _id: users.length + 1 
+
+  })
+
+  res.json({...req.body,
+
+    _id: users.length})
+
+
+})
+
+
+
+app.put('/users/:id', (req, res) => {
+
+  let user = users.find((user) => user._id == req.params.id);
+
+
+  user = {
+    ...user,
+    ...req.body,
+    _id: user._id
+  }
+
+  users[user._id -1] = user;
+
+  console.log('users', users);
+
+  res.send(user);
+
+
+
+})
 
 /* END - create routes here */
 
